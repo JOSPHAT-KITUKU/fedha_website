d5526f5101864153fb1292feccb8101c182424fe
JOSPHAT-KITUKU <kitukujosphat@gmail.com>
2025-04-18 10:06:09 +0300

    Swiper slider for achievements

--- scripts.js
+++ scripts.js
@@ -13,3 +13,28 @@ links.forEach(link => {
     menudis.classList.remove("active");
   });
 });
+
+///Swiper
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,       // Show 3 slides at once
+    spaceBetween: 10,       // Spacing between slides
+    loop: true,             // Loop infinitely
+    autoplay: {
+      delay: 6000,          // Delay between transitions (in ms)
+      disableOnInteraction: false, // Keep autoplay running even after interactions
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+  });
+});
